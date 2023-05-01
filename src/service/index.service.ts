7b69cf8780744a88f521e8021dcadc246ae80e12
Fabien Sabri <fabien.sabri@niji.fr>
2023-05-01 21:43:38 +0200

    production mode

--- src/service/index.service.ts
+++ src/service/index.service.ts
@@ -5,5 +5,5 @@ export * from "./menu.service"
 export * from "./schedule.service"
 export * from "./reservation.service"
 
-export const URL_SCHEME = "http://localhost:8080";
+export const URL_SCHEME = "https://quaiantiqueback-production.up.railway.app";
 export const URL_LOCALHOST = "http://localhost:8080";
